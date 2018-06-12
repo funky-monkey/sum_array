3f48bfb9a70d5b77b073139a1180e14c4a9b42f9
JanPaul <janpaul@elidon.io>
2018-06-12 11:33:10 +0200

    simplified Java

--- src/sumarray/SumArray.java
+++ src/sumarray/SumArray.java
@@ -4,12 +4,13 @@ import java.util.stream.Stream;
 
 public class SumArray {
     public static void main(String[] args) {
-        final Integer[] myArray = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        final Integer[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
         final int sum = Stream.of(myArray)
-                .filter(x -> (x%2)==1)
-                .map(x -> x*2)
-                .reduce(0, (v,acc)-> acc+v);
+                .filter(x -> (x % 2) == 1)
+                .map(x -> x * 2)
+//                .reduce(0, (v,acc)-> acc+v);
+                .reduce(0, Integer::sum);
         System.out.println(sum);
 
     }
